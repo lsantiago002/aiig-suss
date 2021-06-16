47bf3b964ae9d51a9537ec60d9c508e6cdd427da
wtlow003 <wtlow003@suss.edu.sg>
2021-06-17 02:29:11 +0800

    Build: :sparkles: Write custom transformers and functions for preprocessing pipeline

--- learning_materials/regression/src/features/preprocessing.py
+++ learning_materials/regression/src/features/preprocessing.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
 
 class ImputeNans(BaseEstimator, TransformerMixin):
@@ -13,8 +16,6 @@ class ImputeNans(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         categorical_columns = [col for col in X.columns if X.dtypes[col] == 'object']
         numerical_columns = [col for col in X.columns if col not in categorical_columns]
-        # sanity check: categorical + numerical = total
-        assert(len(categorical_columns) + len(numerical_columns) == len(X)))
 
         for col in categorical_columns + numerical_columns:
             if col in categorical_columns:
@@ -38,3 +39,19 @@ class GetAge(BaseEstimator, TransformerMixin):
         X.drop(['YearBuilt'], axis=1 , inplace=True)
 
         return X
+
+def make_dataset(df, y):
+    output_feature = df[y]
+    input_features = df.drop(y, axis=1)
+
+    return input_features, output_feature
+
+
+def preprocess_pipeline(X):
+    categorical_columns = [col for col in X.columns if X.dtypes[col] == 'object']
+    numerical_columns = [col for col in X.columns if col not in categorical_columns]
+
+    return make_column_transformer(
+        (make_pipeline(ImputeNans(), GetAge(), MinMaxScaler()), numerical_columns),
+        (make_pipeline(ImputeNans(), OneHotEncoder()), categorical_columns)
+    )

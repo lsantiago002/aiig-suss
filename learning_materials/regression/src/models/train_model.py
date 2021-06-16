f1316af0d30d93d5eec2d13960dcfe754d88d31c
wtlow003 <wtlow003@suss.edu.sg>
2021-06-17 02:29:49 +0800

    Build: :sparkles: Write code for model training and evaluation

---
+++ learning_materials/regression/src/models/train_model.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import r2_score, mean_squared_error
+
+def train_model(predictors, target, model_class):
+
+    X_train, X_test, y_train, y_test = train_test_split(predictors, target, test_size=0.2, random_state=42)
+
+    model_class.fit(X_train, y_train)
+    y_pred = model_class.predict(X_test)
+
+    # Cross-valiation score
+    print("Training model on 10-Fold Cross Validation...")
+    print(f"CV Score: {np.mean(cross_val_score(model_class, X_train, y_train, scoring='r2', cv=10)):.2f}\n")
+
+    return model_class, y_pred, y_test
+
+def evaluate_model(predictions, actual, model_class):
+
+    # Generating evaluation metrics
+    rmse = np.sqrt(mean_squared_error(actual, predictions))
+    r2 = r2_score(actual, predictions)
+
+    print("Results on Test Data")
+    print("====================")
+    print(f"RMSE: {rmse:.2f}")
+    print(f"R2 Score: {r2:.5f}")
+

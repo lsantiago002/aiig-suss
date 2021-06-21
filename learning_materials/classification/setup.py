c47d5abad583b91526b9f378a098a4229ce7341a
wtlow003 <wtlow003@suss.edu.sg>
2021-06-21 18:07:39 +0800

    :tada: Initial commit

---
+++ learning_materials/classification/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project repo for SUP-4: Classification.',
+    author='SUSS-AIIG',
+    license='MIT',
+)

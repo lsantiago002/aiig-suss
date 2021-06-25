c405ac086d3b304e34a4eb95defaff6d52a00ca8
wtlow003 <wtlow003@suss.edu.sg>
2021-06-26 00:30:02 +0800

    Minor amendments

--- learning_materials/regression/src/models/train_model.py
+++ learning_materials/regression/src/models/train_model.py
@@ -4,9 +4,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import r2_score, mean_squared_error
 
+
 def train_model(predictors, target, model_class):
 
-    X_train, X_test, y_train, y_test = train_test_split(predictors, target, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        predictors, target, test_size=0.2, random_state=42
+    )
 
     model_class.fit(X_train, y_train)
     y_pred = model_class.predict(X_test)
@@ -15,6 +18,7 @@ def train_model(predictors, target, model_class):
     print("Training model...")
 
     return model_class, y_pred, y_test
+
 
 def evaluate_model(predictions, actual, model_class):
 
@@ -26,4 +30,3 @@ def evaluate_model(predictions, actual, model_class):
     print("====================")
     print(f"RMSE: {rmse:.2f}")
     print(f"R2 Score: {r2:.5f}")
-
